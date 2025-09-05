e3bea327500bc603a83c4ccd694c5ea5259a7563
Jordan Layao <58056065+jordanlayao@users.noreply.github.com>
2025-09-05 08:48:04 -0400

    Added modals

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+import { menuArray } from '/data.js'
 
+function getMenuArray(array){
+  return array.map(property => {
+  
+    const {
+      name,
+      ingredients,
+      price,
+      image
+    } = property
+    
+    return `
+    <section class="item">
+      <div class="item-image">
+        <img src="images/${image}" alt="pizza">
+      </div>
+      <div class="item-details">
+        <div class="item-details-container">
+          <div class="detail-header">
+              <p class="item-title">${name}</p>
+              <p class="item-ingredients">${ingredients}</p>
+          </div>
+          <div class="detail-footer">
+            <p class="item-price">$${price}</p>
+          </div>
+      </div>    
+        <button class="add-button">+</button>             
+      </div>
+  </section>`
+  
+}).join('')
+}
+
+document.getElementById('main-container').innerHTML = getMenuArray(menuArray)
